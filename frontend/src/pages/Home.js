b0476087c7bf6de6f298d04f9a2bb160ddac6dae
Nithiya Sri <nithiyasri.r2024aids@sece.ac.in>
2025-12-17 15:57:25 +0530

    hi file is add
    ci cd

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -35,7 +35,7 @@ const Home = () => {
     return (
       <div className="container mx-auto px-4 py-8">
         <div className="flex justify-center items-center h-64">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-red-600"></div>
         </div>
       </div>
     );
